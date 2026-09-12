bc0f58b6fa6273d9ed3d43ab90748afdbfcb1863
eylon maayan <eylonmaayan@gmail.com>
2026-09-12 10:24:19 +0300

    fix: express v5 wildcard route syntax

--- backend/server.js
+++ backend/server.js
@@ -33,11 +33,12 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationsRoutes);
 
+
 // Frontend Production Serving
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*splat", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
