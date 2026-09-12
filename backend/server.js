ca3cbc023ab9afcf3877107421fedff176a89ca4
eylon maayan <eylonmaayan@gmail.com>
2026-09-12 10:22:17 +0300

    fix: serve react frontend in production

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser"; // 1. ייבוא של cookie-parser
-import { v2 as cloudinary} from "cloudinary";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
-import connectMongoDB from "./db/connectMongoDB.js";
 import notificationsRoutes from "./routes/notifications.route.js";
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
@@ -18,17 +20,27 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
-app.use(cookieParser()); // 2. שימוש ב-cookieParser (חובה לפני ה-routes)
+app.use(cookieParser());
 
-// Routes
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationsRoutes);
+
+// Frontend Production Serving
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
